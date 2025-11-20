880c9475b2dd6e727c9672aedb135f9178baa6e5
hshehriyaralam <hshehriyaralam@gmail.com>
2025-11-20 16:45:50 +0500

    add color and font variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layouts/navbar";
 import Footer from "./components/layouts/footer";
+import Home from "./pages/home";
 
 
 
@@ -11,7 +12,7 @@ function App() {
     <BrowserRouter>
     <Navbar />
     <Routes>
-      <Route path="/" element={<h1>Home Page</h1>} />
+      <Route path="/" element={<Home />} />
     </Routes> 
 
     <Footer />
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,10 @@
+
+
+const Home = () => {
+  return (
+    <div className="text-center text-accent  px-10  py-10    font-heading    "  >Stay home & get your daily
+needs from our shop</div>
+  )
+}
+
+export default Home
