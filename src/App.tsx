171681680ff2410f54dd2fdf7f8bf952ec8c9a3b
hshehriyaralam <hshehriyaralam@gmail.com>
2025-11-20 23:19:03 +0500

    add components folders

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layouts/navbar";
 import Footer from "./components/layouts/footer";
 import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact/indes";
+import Blog from "./pages/blog";
 
 
 
@@ -13,6 +16,10 @@ function App() {
     <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/blog" element={<Blog />} />
+
     </Routes> 
 
     <Footer />
