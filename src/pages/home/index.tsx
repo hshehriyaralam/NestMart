2eb032e47b74409625da842e0b26e8f4b2289d0c
hshehriyaralam <hshehriyaralam@gmail.com>
2025-11-20 23:07:07 +0500

    update pages

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,7 +2,8 @@
 
 const Home = () => {
   return (
-    <div className="text-center text-accent  px-10  py-10    font-heading    "  >Stay home & get your daily
+    <div className="text-center text-accent  px-10  py-10 font-heading">
+      Stay home & get your daily
 needs from our shop</div>
   )
 }
