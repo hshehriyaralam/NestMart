112d6042087102962b018576e4aada44b747c536
Hafiz Shehriyar Alam <154017155+hshehriyaralam@users.noreply.github.com>
2025-11-24 00:14:30 +0500

    Revamp navbar and banner UI, add category dropdowns
    Improved the navigation and categories components with enhanced dropdowns, selection state, and badge counters for actions. Updated Banner styling and logic for better layout and border handling. Added a new Header-Logo.svg and updated favicon reference. Introduced type definitions for navigation items in src/types/home.d.ts.

--- src/components/commons/Banner.tsx
+++ src/components/commons/Banner.tsx
@@ -12,23 +12,30 @@ const Banner = () => {
 
 
   return (
-    <div className="w-[92%] mx-auto h-10 hidden lg:flex justify-between items-center bg-transparent
-     ">
+    <div className="w-[95%] mx-auto h-10 hidden lg:flex justify-between items-center bg-transparent p-4
+     border-b border-gray-200">
       <div >
         <ul className="flex  items-center gap-2">
           {navigationItems.map((item) => (
-            <li 
-              className={`action-button-text text-xs gap-4
-                 font-medium ${item.hasBorder ? 'border-l pl-1' : ''}`} 
-              key={item.label}
-            >
-              {item.label}
+            // <li 
+            //   className={`action-button-text text-xs gap-4    
+            //      font-medium ${item.hasBorder ? 'border-l border-gray-200  pl-2' : ''}`} 
+            //   key={item.label}
+            // >
+            //   {item.label}
+            // </li>
+            <li className="flex items-center">
+              {item.hasBorder && <div className="h-2 w-[1px] bg-gray-200 mr-2"></div>}
+              <span className={`action-button-text text-xs gap-4  font-medium   `} >{item.label}</span>
             </li>
           ))}
         </ul>
       </div>
       <div>
-        <p className="action-button-text text-sm font-medium ">100% Secure delivery without contacting the courier</p>
+        <p 
+        // className="action-button-text text-[14px] font-medium "
+        className="text-accent  text-[14px] font-normal [word-spacing-[10px]]  px-2 "
+        >100% Secure delivery without contacting the courier</p>
       </div>
 
       <div>
