73edfdf72ca972f82934d1b229041f5adde17cf9
hshehriyaralam <hshehriyaralam@gmail.com>
2026-01-18 22:32:44 +0500

    Refactor auth pages and update login/signup UI
    Moved and updated login and signup form components to a new 'auth' directory. Redesigned the login and signup pages with new layouts, animated image transitions, and updated form styling. Added new image assets and removed old ones. Introduced Poppins font to index.css.

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,11 +1,86 @@
-import { LoginForm } from '@/components/login/login-form'
+import { Button } from '@/components/ui/button'
+import { useEffect, useState } from 'react';
 
 const Login = () => {
+  const [transition, setTransition] = useState(false);
+
+  
+useEffect(() => {
+    const interval = setInterval(() => {
+      setTransition(prev => !prev);
+    }, 2000); 
+
+    return () => clearInterval(interval); 
+  }, []);
+
+
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <LoginForm />
+
+  <div className='w-full h-min-screen  ' >
+
+
+    <div className='w-full h-full  flex  items-center justify-center  p-6  bg-white' >
+
+
+
+    {/* Login Form */}
+    <div  className='w-[40%]   p-8  ' >
+      <div  className='p-6  '>
+        {/* Logo */}
+        <div className='w-[300px] mx-5' >
+          <img src="/logo/Nest.svg" alt="" />
+        </div>
+
+        <div className=' p-6 flex flex-col gap-y-3 ' >
+          <p  className='font-quicksand   text-secondary  text-sm '>Welcome back !! </p>
+          <h2 className='font-heading text-4xl' >Sign in</h2>
+          
+          <form  className='flex flex-col my-2'>
+          <label htmlFor="email"  className='font-quicksand text-secondary my-1 mt-2'>Email</label>
+          <input type="email"  placeholder='Enter Email' className='bg-[#F4FFF5] font-quicksand text-accent outline-none px-4 py-2  rounded' />
+
+          
+          <label htmlFor="email" className='font-quicksand text-secondary my-1 mt-4'>Password</label>
+          <input type="password"  placeholder='Enter Password'  className='bg-[#F4FFF5] font-quicksand text-accent outline-none px-4 py-2 rounded ' />
+
+
+          <Button  className='w-[150px]  rounded-full bg-primary  text-white
+           font-quicksand mt-4 hover:bg-primary/70  cursor-pointer mx-auto '
+           type='submit'
+           >
+            Sign In
+          </Button>
+
+          <p  className='font-quicksand  text-accent text-[14px] text-center mt-4'>
+            I don't have an account?
+             <a href="/auth/signup" className="text-primary  font-semibold"> Sign up</a>
+             </p>
+          </form>
+
+        </div>
       </div>
+    </div>
+
+
+
+    {/* Image Section */}
+    <div className='w-[40%]'>
+          <img
+            src={transition ? "/loginTwo.webp" : "/loginOne.webp"}
+            alt="login"
+            className="transition-all duration-700 ease-in-out"
+          />
+        </div>
+
+
+
+  </div>
+
+
+
+
+
+
     </div>
   )
 }
--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -1,11 +1,89 @@
-import { SignupForm } from '@/components/signup/signup-form'
+import { Button } from '@/components/ui/button'
+import { useEffect, useState } from 'react';
 
 const Signup = () => {
+  const [transition, setTransition] = useState(false);
+
+  
+useEffect(() => {
+    const interval = setInterval(() => {
+      setTransition(prev => !prev);
+    }, 2000); 
+
+    return () => clearInterval(interval); 
+  }, []);
+
+
   return (
-      <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <SignupForm />
+
+  <div className='w-full h-min-screen  ' >
+
+
+    <div className='w-full h-full  flex  items-center justify-center  p-6  bg-white' >
+
+
+
+    {/* Login Form */}
+    <div  className='w-[40%] h-[533px]      p-4   ' >
+      <div  className='p-6  '>
+        {/* Logo */}
+        <div className='w-[300px] mx-5' >
+          <img src="/logo/Nest.svg" alt="" />
+        </div>
+
+        <div className=' p-6 flex flex-col gap-y-3 ' >
+          <p  className='font-quicksand   text-secondary  text-sm '>Welcome to Nest!  </p>
+          <h2 className='font-heading text-4xl' >Sign up</h2>
+          
+          <form  className='flex flex-col my-2'>
+          <label htmlFor="fullName"  className='font-quicksand text-secondary my-1 mt-2'>Full Name</label>
+          <input type="text"  placeholder='Enter Full Name' className='bg-[#F4FFF5] font-quicksand text-accent outline-none px-4 py-2  rounded' />
+
+            <label htmlFor="email"  className='font-quicksand text-secondary my-1 mt-2'>Email</label>
+          <input type="email"  placeholder='Enter Email' className='bg-[#F4FFF5] font-quicksand text-accent outline-none px-4 py-2  rounded' />
+
+          
+          <label htmlFor="email" className='font-quicksand text-secondary my-1 mt-4'>Password</label>
+          <input type="password"  placeholder='Enter Password'  className='bg-[#F4FFF5] font-quicksand text-accent outline-none px-4 py-2 rounded ' />
+
+
+          <Button  className='w-[150px]  rounded-full bg-primary  text-white
+           font-quicksand mt-4 hover:bg-primary/70  cursor-pointer mx-auto '
+           type='submit'
+           >
+            Sign In
+          </Button>
+
+          <p  className='font-quicksand  text-accent text-[14px] text-center mt-4'>
+            I have an account?
+             <a href="/auth/login" className="text-primary  font-semibold"> Login</a>
+             </p>
+          </form>
+
+        </div>
       </div>
+    </div>
+
+
+
+    {/* Image Section */}
+    <div className='w-[40%]'>
+          <img
+            src={transition ? "/loginTwo.webp" : "/loginOne.webp"}
+            alt="login"
+            className="transition-all duration-700 ease-in-out"
+          />
+        </div>
+
+
+
+  </div>
+
+
+
+
+
+
     </div>
   )
 }
